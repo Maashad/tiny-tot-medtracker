5906c8c857324669399ba3e66709335a0ed6578b
Amber Shay <mashaad21@gmail.com>
2023-08-10 22:42:11 -0600

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <h4>© 2023 Tiny Tot MedTracker</h4>
+        </footer>
+    );
+}
+
+export default Footer;
