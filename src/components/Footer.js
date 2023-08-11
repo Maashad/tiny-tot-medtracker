17c90d4df84e875c93efd8c62d18092b64c7a8a5
Amber Shay <mashaad21@gmail.com>
2023-08-10 22:58:52 -0600

    update footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,9 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <footer className="footer">
+            <div className="contact">
+                <a href="mailto:tinytotwebmaster@gmail.com?subject='Issue/Suggestion'">Contact Us</a>
+            </div>
             <h4>© 2023 Tiny Tot MedTracker</h4>
         </footer>
     );
