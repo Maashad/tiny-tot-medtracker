ba2a06704581ac42230ba8f0c3e95bdcbaf7b6c7
Amber Shay <mashaad21@gmail.com>
2023-08-11 21:53:27 -0600

    remove firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { initializeApp } from "firebase/app";
 import './index.css';
 import App from './App';
 
@@ -10,15 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCBGkg-9BbkBlWDu_LjxC1CugAtXJVky4Q",
-  authDomain: "tiny-tot.firebaseapp.com",
-  databaseURL: "https://tiny-tot-default-rtdb.firebaseio.com",
-  projectId: "tiny-tot",
-  storageBucket: "tiny-tot.appspot.com",
-  messagingSenderId: "985262874098",
-  appId: "1:985262874098:web:9d5aaef0b64f8a77781f6b"
-};
-
-initializeApp(firebaseConfig);
